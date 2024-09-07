16e7dbfd89611a4c9450d9b5d09f71b3aa762f7c
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2024-09-08 00:40:14 +0200

    feat(wip): some img

--- src/utils/input.py
+++ src/utils/input.py
@@ -4,5 +4,6 @@ import io
 
 def define_string_as_input(string: str) -> None:
     """Tout appel à "input" ou sys.stdin.readline lira en réalité cette string."""
+    global input
     sys.stdin = io.StringIO(string)
     input = io.StringIO(string)
